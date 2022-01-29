271b8f6e6762a260bd8ea51fb87e2b43f5f09b96
artur <arturguimaraesdefreitas@gmail.com>
2022-01-29 20:03:03 -0300

    retirando consoles.logs que estavam apenas por verificações

--- script.js
+++ script.js
@@ -8,22 +8,12 @@ form.validate({
 });
 
 $.validator.addMethod(
-    /* The value you can use inside the email object in the validator. */
     "regex",
-
-    /* The function that tests a given string against a given regEx. */
     function(value, element, regexp)  {
-        /* Check if the value is truthy (avoid null.constructor) & if it's not a RegEx. (Edited: regex --> regexp)*/
-
         if (regexp && regexp.constructor != RegExp) {
-           /* Create a new regular expression using the regex argument. */
            regexp = new RegExp(regexp);
         }
-
-        /* Check whether the argument is global and, if so set its last index to 0. */
         else if (regexp.global) regexp.lastIndex = 0;
-
-        /* Return whether the element is optional or the result of the validation. */
         return this.optional(element) || regexp.test(value);
     }
 );
@@ -38,11 +28,8 @@ $("#form").submit(function (e) {
 
 $("#limpar").click(function (e) { 
     e.preventDefault();
-
-  
     deletar();
     
-    get()
 });
 
 function deletar(){
@@ -52,10 +39,9 @@ function deletar(){
         success: function(response){
             
             $("#salvo").html(response) 
-            
             $('#exampleModal').modal('show');
         
-
+            get()
         },
         dataType:"json"
 
@@ -64,6 +50,7 @@ function deletar(){
 }
 
 function post(){
+    
     form.validate({
         email: {
             required: true,
@@ -71,59 +58,61 @@ function post(){
             regex: /^\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b$/i
         }
     });
-            if(form.valid()==true){
-                var u_nome = $('#nome').val();
-                var u_email = $('#email').val();
-                var u_cpf = $('#cpf').val();
-                var cpf = CryptoJS.AES.encrypt(u_cpf,"123")
-        
-                console.log(cpf.toString())
-        
-                $.ajax({
-                    type: "POST",
-                    url: "http://localhost/ListaPHP/connection.php",
-                    data: {nome:u_nome, email:u_email, cpf:cpf.toString()},
-                    success: function(response){
-                        console.log(response)
-                        if(response!="salvo"){
-                            $("#salvo").html(response) 
-                                  
-                        }else{
-                            get();
-                            $("#salvo").html("salvo com sucesso")
-                            document.getElementById("nome").value="";
-                            document.getElementById("cpf").value="";
-                            document.getElementById("email").value="";
+
+
+    if(form.valid()==true){
+        var u_nome = $('#nome').val();
+        var u_email = $('#email').val();
+        var u_cpf = $('#cpf').val();
+        var cpf = CryptoJS.AES.encrypt(u_cpf,"123")
+
+        $.ajax({
+            type: "POST",
+            url: "http://localhost/ListaPHP/connection.php",
+            data: {nome:u_nome, email:u_email, cpf:cpf.toString()},
+            success: function(response){
+
+                if(response!="salvo"){
+                    $("#salvo").html(response) 
                             
-                        }
-                        $('#exampleModal').modal('show');
-                        
-                    },
-                    dataType: "json"
-                   
-                });
-            }
+                }else{
+
+                    
+                    $("#salvo").html("salvo com sucesso")
+                    document.getElementById("nome").value="";
+                    document.getElementById("cpf").value="";
+                    document.getElementById("email").value="";
+                    get();
+                }
+
+                $('#exampleModal').modal('show');
+                
+            },
+            dataType: "json"
             
-        }
+        });
+    }
+            
+}
 
         
-        function get(){
+function get(){
+    
+    $.get("http://localhost/ListaPHP/connection.php", function (data) {
+
+        var resultado = JSON.parse(data);
+
+        $(".list").html("<thead class='table-dark'><tr><td>Nome</td><td>Email</td><td>CPF</td></tr> </thead>");
+
+        for (var i = 0; i < resultado.length; i++) {
             
-            $.get("http://localhost/ListaPHP/connection.php", function (data) {
-                var resultado = JSON.parse(data);
-                $(".list").html("<thead class='table-dark'><tr><td>Nome</td><td>Email</td><td>CPF</td></tr> </thead>");
-                console.log(resultado);
-        
-                for (var i = 0; i < resultado.length; i++) {
-                    
-                    var decrypted = CryptoJS.AES.decrypt(resultado[i].cpf, "123");
-        
-        
-                        $(".list").append("<tr><td>" + resultado[i].nome + "<td>" + resultado[i].email + "</td>"+"<td>" + decrypted.toString(CryptoJS.enc.Utf8) +"</td></tr>");
-                   
-                }
-            });
+            var decrypted = CryptoJS.AES.decrypt(resultado[i].cpf, "123");
+            
+            $(".list").append("<tr><td>" + resultado[i].nome + "<td>" + resultado[i].email + "</td>"+"<td>" + decrypted.toString(CryptoJS.enc.Utf8) +"</td></tr>");
+            
         }
+    });
+}
 
 
 get();
